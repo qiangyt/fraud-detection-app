8f4d42c0c10dfefb573fcb987ac02ad795ba3af6
Yiting Qiang <qiangyt@wxcount.com>
2024-12-10 10:53:56 +0800

    tests SqsAlerter

---
+++ src/test/java/qiangyt/fraud_detection/app/alert/SqsAlerterTest.java
@@ -0,0 +1,69 @@
+package qiangyt.fraud_detection.app.alert;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import qiangyt.fraud_detection.app.config.SqsProps;
+import qiangyt.fraud_detection.framework.json.Jackson;
+import qiangyt.fraud_detection.sdk.DetectionReqEntity;
+import qiangyt.fraud_detection.sdk.DetectionResult;
+import qiangyt.fraud_detection.sdk.FraudCategory;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.Date;
+
+public class SqsAlerterTest {
+
+    @Mock
+    private SqsProps props;
+
+    @Mock
+    private SqsClient client;
+
+    @Mock
+    private Jackson jackson;
+
+    @Mock
+    private GroupedAlerter group;
+
+    @InjectMocks
+    private SqsAlerter alerter;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(props.getAlertQueueUrl()).thenReturn("test-queue-url");
+        alerter.init();
+    }
+
+    @Test
+    public void testSend() {
+        var entity = DetectionReqEntity.builder().accountId("account-id").amount(123).memo("memo").id("entity-id").receivedAt(new Date()).build(); 
+        var result = DetectionResult.from(entity, FraudCategory.SUSPICIOUS_ACCOUNT);
+        
+        when(jackson.str(result)).thenReturn("test-message");
+
+        alerter.send(result);
+
+        var captor = ArgumentCaptor.forClass(SendMessageRequest.class);
+        verify(client).sendMessage(captor.capture());
+
+        var req = captor.getValue();
+        assertEquals("test-queue-url", req.queueUrl());
+        assertEquals("test-message", req.messageBody());
+        assertEquals("entity-id", req.messageDeduplicationId());
+        assertEquals("alert", req.messageGroupId());
+    }
+
+    @Test
+    public void testInit() {
+        verify(group).registerAlerter(alerter);
+    }
+}
