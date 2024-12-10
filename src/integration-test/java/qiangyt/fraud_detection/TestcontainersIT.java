04fc79e62d111e15bf3dd43dd526a67afde4d57a
Yiting Qiang <qiangyt@wxcount.com>
2024-12-10 21:13:38 +0800

    intializing testcontainers

---
+++ src/integration-test/java/qiangyt/fraud_detection/TestcontainersIT.java
@@ -0,0 +1,42 @@
+/*
+ * fraud-detection-app - fraud detection app
+ * Copyright © 2024 Yiting Qiang (qiangyt@wxcount.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package qiangyt.fraud_detection;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+//import org.testcontainers.containers.localstack.LocalstackTestImages;
+
+@Testcontainers
+public class TestcontainersIT {
+
+    //DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.5.0");
+
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("localstack/localstack:3.5.0");
+
+    @ClassRule
+    public static LocalStackContainer localstack = new LocalStackContainer(DEFAULT_IMAGE_NAME)
+            .withServices(
+                    Service.SQS,
+                    Service.CLOUDWATCHLOGS,
+                    LocalStackContainer.EnabledService.named("events"));
+
+}
