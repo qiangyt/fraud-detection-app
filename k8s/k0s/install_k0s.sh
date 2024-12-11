d98bdcf4f36d1753437bb74d0f13e52454d799ad
Yiting Qiang <qiangyt@wxcount.com>
2024-12-12 01:07:12 +0800

    Revert "clean pom"
    This reverts commit 7e59778c1bf0d850b33910d34ad6e2856bffa95e.

--- k8s/k0s/install_k0s.sh
+++ k8s/k0s/install_k0s.sh
@@ -51,10 +51,7 @@ main() {
 
   echo "Downloading k0s from URL: $k0sDownloadUrl"
 
-  if ! curl -sSL "$k0sDownloadUrl" >"$k0sInstallPath/$k0sBinary"; then
-    echo "Error: Failed to download k0s from $k0sDownloadUrl" 1>&2
-    exit 1
-  fi
+  curl -sSLf "$k0sDownloadUrl" >"$k0sInstallPath/$k0sBinary"
   chmod 755 -- "$k0sInstallPath/$k0sBinary"
 
   echo "k0s is now executable in $k0sInstallPath"
